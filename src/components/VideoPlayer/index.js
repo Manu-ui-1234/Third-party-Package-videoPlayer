92529f8961e21852e428ff0e9fe337b25304f2c9
Manu-ui-1234 <arrammanu08@gmail.com>
2024-12-25 21:06:51 +0530

    Update index.js

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -2,12 +2,13 @@ import ReactPlayer from 'react-player'
 
 import './index.css'
 
+const videoURL = 'https://youtu.be/jOst7Trpj7o?si=hSsmI_BIVH-OhMZO'
 const VideoPlayer = () => (
   <div className="video-container">
     <h1 className="heading">Video Player</h1>
     <div className="responsive-container">
       <ReactPlayer
-        url="https://youtu.be/jOst7Trpj7o?si=hSsmI_BIVH-OhMZO"
+        url={videoURL}
         controls
         light
       />
